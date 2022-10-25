8db965043be45edfb00a52aaa8ceb2cfaeabff9b
Vencent <aqqq15219@gmail.com>
2022-10-25 12:10:15 +1100

    Changes to be committed: 	modified:   index.js

--- index.js
+++ index.js
@@ -1,54 +1,44 @@
-import fetch from 'node-fetch';
-import { stdin, stdout } from 'process';
-import readline from 'readline';
+import fetch from "node-fetch";
+import { stdin, stdout } from "process";
+import readline from "readline";
 
-const compare = ( a, b ) => {
-    if ( a.API < b.API ){
-      return 1;
-    }
-    if ( a.API > b.API ){
-      return -1;
-    }
-    return 0;
+const compare = (a, b) => {
+  if (a.API < b.API) {
+    return 1;
   }
+  if (a.API > b.API) {
+    return -1;
+  }
+  return 0;
+};
 
 fetch("https://api.publicapis.org/entries")
-.then(response => response.json())
-.then(json => {
+  .then((response) => response.json())
+  .then((json) => {
     let arrayEntries = [];
     let sortedArray = [];
     json.entries.map((entry) => arrayEntries.push(entry));
-    sortedArray = arrayEntries.sort((a, b) => compare(a,b));
-    // console.log(sortedArray);
+    sortedArray = arrayEntries.sort((a, b) => compare(a, b));
 
-    // const argu = process.argv.slice(2);
-
-    const input = [];
     const rl = readline.createInterface({
-        input:stdin,
-        output:stdout
+      input: stdin,
+      output: stdout,
     });
 
-    rl.question('Please enter the category name : ', (cate) => {
-        rl.question('Please enter the number of results you want to see : ', (limit) => {
-            console.log(sortedArray.filter(result => result.Category === cate).slice(0,limit));
-            
-            rl.close();
-        });
+    rl.question("Please enter the category name : ", (cate) => {
+      rl.question(
+        "Please enter the number of results you want to see : ",
+        (limit) => {
+          const print = sortedArray
+            .filter((result) => result.Category === cate)
+            .slice(0, limit);
+          print.length == 0
+            ? console.log(
+                "No record, please enter the correct category name(case sensitive)"
+              )
+            : console.log(print);
+          rl.close();
+        }
+      );
     });
-
-    // rl.prompt();
-
-    // rl.on('line',function(cmd){
-    //     input.push(cmd);
-    // })
-
-    // rl.on('close', function(cmd){
-    //     console.log(input.join('/n'));
-    //     process.exit(0);
-    // })
-
-});
-
-
-
+  });
